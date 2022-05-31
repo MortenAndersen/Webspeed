20e2929243226eb2e1b8d40c892dc527a72d907a
Morten Andersen <morten@hjemmesider.dk>
2022-05-31 11:27:22 +0200

    elements
    elements no search

--- inc/post-type.php
+++ inc/post-type.php
@@ -13,7 +13,11 @@ add_action( 'init', 'create_posttype_webspeed_elements' );
 	    			'singular_name' => __('Element', 'webspeed-domain')
 	    		),
 	    		'public' => true,
-	    		'publicly_queryable' => false,
+	    		'show_in_admin_bar'   => true,
+				'show_in_nav_menus'   => false,
+				'publicly_queryable'  => false,
+				'query_var'           => false,
+				'exclude_from_search' => true,
 	    		'menu_icon' => 'dashicons-format-gallery',
 	    		'supports' => array(
 	    			'title',
