4722462ddecddd16ea65ab4a72b94e905fe9a400
Morten Andersen <morten@hjemmesider.dk>
2022-09-23 12:12:39 +0200

    kontakt og personer
    the same

--- template-parts/gutenberg-blocks/contact/contact.php
+++ template-parts/gutenberg-blocks/contact/contact.php
@@ -1,37 +1,35 @@
 <?php
 
-
-
-echo '<ul class="contact-con">';
+echo '<ul class="kontakt-con">';
 
 	if( get_field('adresse') ) {
-    	echo '<li class="contact-location"><span class="contact-label">';
+    	echo '<li class="k-loca"><span class="k-label">';
     	   echo svg_url(5);
-    	echo '</span><span class="contact-info">' . get_field('adresse') . '</span></li>';
+    	echo '</span><span class="k-info">' . get_field('adresse') . '</span></li>';
 	}
 
 	if( get_field('email') ) {
-    	echo '<li class="contact-email"><span class="contact-label">';
+    	echo '<li class="k-mail"><span class="k-label">';
     	   echo svg_url(2);
-    	echo '</span><span class="contact-info"><a href="mailto:' . get_field('email') . '">' . get_field('email') . '</a></span></li>';
+    	echo '</span><span class="k-info"><a href="mailto:' . get_field('email') . '">' . get_field('email') . '</a></span></li>';
     }
     if( get_field('telefon') ) {
-    	echo '<li class="contact-telefon"><span class="contact-label">';
+    	echo '<li class="k-tel"><span class="k-label">';
     	   echo svg_url(1);
-    	echo '</span><span class="contact-info"><a href="tel:' . str_replace(' ', '',get_field('telefon')) . '">' . get_field('telefon') . '</a></span></li>';
+    	echo '</span><span class="k-info"><a href="tel:' . str_replace(' ', '',get_field('telefon')) . '">' . get_field('telefon') . '</a></span></li>';
     }
     if( get_field('telefon_2') ) {
-    	echo '<li class="contact-telefon-2"><span class="contact-label">';
+    	echo '<li class="k-tel-2"><span class="k-label">';
     	   echo svg_url(1);
-    	echo '</span><span class="contact-info"><a href="tel:' . str_replace(' ', '',get_field('telefon_2')) . '">' . get_field('telefon_2') . '</a></span></li>';
+    	echo '</span><span class="k-info"><a href="tel:' . str_replace(' ', '',get_field('telefon_2')) . '">' . get_field('telefon_2') . '</a></span></li>';
     }
     if( get_field('info') ) {
-    	echo '<li class="contact-info"><span class="contact-label"></span><span class="contact-info">' . get_field('info') . '</span></li>';
+    	echo '<li class="k-info"><span class="k-label"></span><span class="k-info">' . get_field('info') . '</span></li>';
     }
 
 
     if( get_field('facebook') || get_field('linkedin') || get_field('instagram') ) {
-        echo '<li class="contact-social">';
+        echo '<li class="k-some">';
             if( get_field('facebook') ) {
                 echo '<a href="' . get_field('facebook') . '" class="fb" target="_blank">' . svg_url(7) . '</a>';
             }
