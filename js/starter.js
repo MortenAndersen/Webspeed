aa6bf57ecd90d1a4e7536cde4a9583fc5b12a58a
Morten Andersen <morten@hjemmesider.dk>
2022-10-11 10:41:01 +0200

    has-bg
    new file

--- js/starter.js
+++ js/starter.js
@@ -147,6 +147,36 @@ $('.m-a-t').click(function() {
             } 
         });
 
+// --------------------------------------------
+
+        // Element i viewpoint - skal forfines!
+
+        function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+$(window).scroll(function () {
+    $('.img-zoom').each(function () {
+        if (isScrolledIntoView(this) === true) {
+            $(this).addClass('visible');
+        }
+    });
+
+});
+
+$('.img-zoom').each(function () {
+        if (isScrolledIntoView(this) === true) {
+            $(this).addClass('visible');
+        }
+    });
+
+// --------------------------------------------
 
 });
 
