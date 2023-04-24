0b9fe95d11cf9fc8b94af8cd788aa47c94b3c2b8
Morten Andersen <morten@hjemmesider.dk>
2023-04-24 13:23:46 +0200

    overskrift woo
    woo title ACF

--- inc/acf-woocommerce.php
+++ inc/acf-woocommerce.php
@@ -53,4 +53,51 @@ if (function_exists('acf_add_local_field_group')):
 		'show_in_rest' => 0,
 	));
 
+// Title
+
+	acf_add_local_field_group(array(
+		'key' => 'group_6421a38da4c5b',
+		'title' => 'Woo Title',
+		'fields' => array(
+			array(
+				'key' => 'field_6421a38dc0034',
+				'label' => 'Overskrift - alt. H1',
+				'name' => 'overskrift',
+				'aria-label' => '',
+				'type' => 'text',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'maxlength' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'product',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+		'show_in_rest' => 0,
+	));
+
 endif;
