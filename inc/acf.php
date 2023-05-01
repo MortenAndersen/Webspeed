2664327e4eaa276a805ce72f542fa58b0015824c
Morten Andersen <morten@hjemmesider.dk>
2023-05-01 15:03:59 +0200

    aside
    placeret over Yoast

--- inc/acf.php
+++ inc/acf.php
@@ -1,280 +1,279 @@
-<?php 
-if( function_exists('acf_add_local_field_group') ):
+<?php
+if (function_exists('acf_add_local_field_group')):
 
-acf_add_local_field_group(array(
-	'key' => 'group_60e81507036ee',
-	'title' => 'Web left',
-	'fields' => array(
-		array(
-			'key' => 'field_60e8155c5acfc',
-			'label' => 'Aside left',
-			'name' => 'aside_left',
-			'type' => 'wysiwyg',
-			'instructions' => '',
-			'required' => 0,
-			'conditional_logic' => 0,
-			'wrapper' => array(
-				'width' => '',
-				'class' => '',
-				'id' => '',
-			),
-			'default_value' => '',
-			'tabs' => 'all',
-			'toolbar' => 'full',
-			'media_upload' => 1,
-			'delay' => 0,
-		),
-	),
-	'location' => array(
-		array(
+	acf_add_local_field_group(array(
+		'key' => 'group_60e81507036ee',
+		'title' => 'Venstre spalte (aside)',
+		'fields' => array(
 			array(
-				'param' => 'page_template',
-				'operator' => '==',
-				'value' => 'page-aside-left-right.php',
+				'key' => 'field_60e8155c5acfc',
+				'label' => 'Aside left',
+				'name' => 'aside_left',
+				'type' => 'wysiwyg',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'tabs' => 'all',
+				'toolbar' => 'full',
+				'media_upload' => 1,
+				'delay' => 0,
 			),
 		),
-		array(
+		'location' => array(
 			array(
-				'param' => 'page_template',
-				'operator' => '==',
-				'value' => 'page-aside-left-right-topimg.php',
+				array(
+					'param' => 'page_template',
+					'operator' => '==',
+					'value' => 'page-aside-left-right.php',
+				),
 			),
-		),
-		array(
 			array(
-				'param' => 'page_template',
-				'operator' => '==',
-				'value' => 'page-aside-left.php',
+				array(
+					'param' => 'page_template',
+					'operator' => '==',
+					'value' => 'page-aside-left-right-topimg.php',
+				),
 			),
-		),
-		array(
 			array(
-				'param' => 'page_template',
-				'operator' => '==',
-				'value' => 'page-aside-left-topimg.php',
+				array(
+					'param' => 'page_template',
+					'operator' => '==',
+					'value' => 'page-aside-left.php',
+				),
 			),
-		),
-		array(
 			array(
-				'param' => 'page_template',
-				'operator' => '==',
-				'value' => 'page-aside-left-titleimg.php',
+				array(
+					'param' => 'page_template',
+					'operator' => '==',
+					'value' => 'page-aside-left-topimg.php',
+				),
 			),
-		),
-	),
-	'menu_order' => 0,
-	'position' => 'normal',
-	'style' => 'default',
-	'label_placement' => 'top',
-	'instruction_placement' => 'label',
-	'hide_on_screen' => '',
-	'active' => true,
-	'description' => '',
-	'show_in_rest' => 0,
-));
-
-// ------------------------------------------------------ //
-
-acf_add_local_field_group(array(
-	'key' => 'group_60e8157739728',
-	'title' => 'Web right',
-	'fields' => array(
-		array(
-			'key' => 'field_60e8158d2ad5a',
-			'label' => 'Aside right',
-			'name' => 'aside_right',
-			'type' => 'wysiwyg',
-			'instructions' => '',
-			'required' => 0,
-			'conditional_logic' => 0,
-			'wrapper' => array(
-				'width' => '',
-				'class' => '',
-				'id' => '',
-			),
-			'default_value' => '',
-			'tabs' => 'all',
-			'toolbar' => 'full',
-			'media_upload' => 1,
-			'delay' => 0,
-		),
-	),
-	'location' => array(
-		array(
 			array(
-				'param' => 'post_template',
-				'operator' => '==',
-				'value' => 'page-aside-left-right.php',
+				array(
+					'param' => 'page_template',
+					'operator' => '==',
+					'value' => 'page-aside-left-titleimg.php',
+				),
 			),
 		),
-		array(
-			array(
-				'param' => 'post_template',
-				'operator' => '==',
-				'value' => 'page-aside-left-right-topimg.php',
-			),
-		),
-		array(
-			array(
-				'param' => 'post_template',
-				'operator' => '==',
-				'value' => 'page-aside-right.php',
-			),
-		),
-		array(
-			array(
-				'param' => 'post_template',
-				'operator' => '==',
-				'value' => 'page-aside-right-topimg.php',
-			),
-		),
-		array(
-			array(
-				'param' => 'post_template',
-				'operator' => '==',
-				'value' => 'page-aside-right-titleimg.php',
-			),
-		),
-	),
-	'menu_order' => 0,
-	'position' => 'normal',
-	'style' => 'default',
-	'label_placement' => 'top',
-	'instruction_placement' => 'label',
-	'hide_on_screen' => '',
-	'active' => true,
-	'description' => '',
-	'show_in_rest' => 0,
-));
+		'menu_order' => -1,
+		'position' => 'acf_after_title',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+		'show_in_rest' => 0,
+	));
 
 // ------------------------------------------------------ //
 
-acf_add_local_field_group(array(
-	'key' => 'group_628b587630961',
-	'title' => 'Theme (Top img)',
-	'fields' => array(
-		array(
-			'key' => 'field_628b58aa031ca',
-			'label' => 'Skjul titel',
-			'name' => 'skjul_titel',
-			'type' => 'true_false',
-			'instructions' => '',
-			'required' => 0,
-			'conditional_logic' => 0,
-			'wrapper' => array(
-				'width' => '',
-				'class' => '',
-				'id' => '',
-			),
-			'message' => '',
-			'default_value' => 0,
-			'ui' => 0,
-			'ui_on_text' => '',
-			'ui_off_text' => '',
-		),
-	),
-	'location' => array(
-		array(
+	acf_add_local_field_group(array(
+		'key' => 'group_60e8157739728',
+		'title' => 'Højre spalte (aside)',
+		'fields' => array(
 			array(
-				'param' => 'post_template',
-				'operator' => '==',
-				'value' => 'page-aside-left-right-topimg.php',
+				'key' => 'field_60e8158d2ad5a',
+				'label' => 'Aside right',
+				'name' => 'aside_right',
+				'type' => 'wysiwyg',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'tabs' => 'all',
+				'toolbar' => 'full',
+				'media_upload' => 1,
+				'delay' => 0,
 			),
 		),
-		array(
+		'location' => array(
 			array(
-				'param' => 'post_template',
-				'operator' => '==',
-				'value' => 'page-aside-left-topimg.php',
+				array(
+					'param' => 'post_template',
+					'operator' => '==',
+					'value' => 'page-aside-left-right.php',
+				),
 			),
-		),
-		array(
 			array(
-				'param' => 'post_template',
-				'operator' => '==',
-				'value' => 'page-aside-right-topimg.php',
+				array(
+					'param' => 'post_template',
+					'operator' => '==',
+					'value' => 'page-aside-left-right-topimg.php',
+				),
 			),
-		),
-		array(
 			array(
-				'param' => 'post_template',
-				'operator' => '==',
-				'value' => 'page-no-wrap-topimg.php',
+				array(
+					'param' => 'post_template',
+					'operator' => '==',
+					'value' => 'page-aside-right.php',
+				),
 			),
-		),
-		array(
 			array(
-				'param' => 'post_template',
-				'operator' => '==',
-				'value' => 'page-topimg.php',
+				array(
+					'param' => 'post_template',
+					'operator' => '==',
+					'value' => 'page-aside-right-topimg.php',
+				),
+			),
+			array(
+				array(
+					'param' => 'post_template',
+					'operator' => '==',
+					'value' => 'page-aside-right-titleimg.php',
+				),
 			),
 		),
-	),
-	'menu_order' => 0,
-	'position' => 'side',
-	'style' => 'default',
-	'label_placement' => 'top',
-	'instruction_placement' => 'label',
-	'hide_on_screen' => '',
-	'active' => true,
-	'description' => '',
-	'show_in_rest' => 0,
-));
+		'menu_order' => 0,
+		'position' => 'acf_after_title',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+		'show_in_rest' => 0,
+	));
 
 // ------------------------------------------------------ //
 
-			if( function_exists('acf_add_local_field_group') ):
-
-acf_add_local_field_group(array(
-	'key' => 'group_629602af09e79',
-	'title' => 'Theme',
-	'fields' => array(
-		array(
-			'key' => 'field_629602b9c12bd',
-			'label' => 'Billede',
-			'name' => 'billede',
-			'type' => 'image',
-			'instructions' => 'Alternativ oversigtsbillede',
-			'required' => 0,
-			'conditional_logic' => 0,
-			'wrapper' => array(
-				'width' => '',
-				'class' => '',
-				'id' => '',
-			),
-			'return_format' => 'id',
-			'preview_size' => 'medium',
-			'library' => 'all',
-			'min_width' => '',
-			'min_height' => '',
-			'min_size' => '',
-			'max_width' => '',
-			'max_height' => '',
-			'max_size' => '',
-			'mime_types' => '',
-		),
-	),
-	'location' => array(
-		array(
+	acf_add_local_field_group(array(
+		'key' => 'group_628b587630961',
+		'title' => 'Theme (Top img)',
+		'fields' => array(
 			array(
-				'param' => 'post_type',
-				'operator' => '==',
-				'value' => 'page',
+				'key' => 'field_628b58aa031ca',
+				'label' => 'Skjul titel',
+				'name' => 'skjul_titel',
+				'type' => 'true_false',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'message' => '',
+				'default_value' => 0,
+				'ui' => 0,
+				'ui_on_text' => '',
+				'ui_off_text' => '',
 			),
 		),
-	),
-	'menu_order' => 0,
-	'position' => 'side',
-	'style' => 'default',
-	'label_placement' => 'top',
-	'instruction_placement' => 'label',
-	'hide_on_screen' => '',
-	'active' => true,
-	'description' => '',
-	'show_in_rest' => 0,
-));
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_template',
+					'operator' => '==',
+					'value' => 'page-aside-left-right-topimg.php',
+				),
+			),
+			array(
+				array(
+					'param' => 'post_template',
+					'operator' => '==',
+					'value' => 'page-aside-left-topimg.php',
+				),
+			),
+			array(
+				array(
+					'param' => 'post_template',
+					'operator' => '==',
+					'value' => 'page-aside-right-topimg.php',
+				),
+			),
+			array(
+				array(
+					'param' => 'post_template',
+					'operator' => '==',
+					'value' => 'page-no-wrap-topimg.php',
+				),
+			),
+			array(
+				array(
+					'param' => 'post_template',
+					'operator' => '==',
+					'value' => 'page-topimg.php',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'side',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+		'show_in_rest' => 0,
+	));
 
-endif;		
+// ------------------------------------------------------ //
 
+	if (function_exists('acf_add_local_field_group')):
+
+		acf_add_local_field_group(array(
+			'key' => 'group_629602af09e79',
+			'title' => 'Theme',
+			'fields' => array(
+				array(
+					'key' => 'field_629602b9c12bd',
+					'label' => 'Billede',
+					'name' => 'billede',
+					'type' => 'image',
+					'instructions' => 'Alternativ oversigtsbillede',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'return_format' => 'id',
+					'preview_size' => 'medium',
+					'library' => 'all',
+					'min_width' => '',
+					'min_height' => '',
+					'min_size' => '',
+					'max_width' => '',
+					'max_height' => '',
+					'max_size' => '',
+					'mime_types' => '',
+				),
+			),
+			'location' => array(
+				array(
+					array(
+						'param' => 'post_type',
+						'operator' => '==',
+						'value' => 'page',
+					),
+				),
+			),
+			'menu_order' => 0,
+			'position' => 'side',
+			'style' => 'default',
+			'label_placement' => 'top',
+			'instruction_placement' => 'label',
+			'hide_on_screen' => '',
+			'active' => true,
+			'description' => '',
+			'show_in_rest' => 0,
+		));
+
+	endif;
 
 endif;
