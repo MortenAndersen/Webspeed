e32088ddf7a1dae2a168177f6d41cbe2bb322004
Morten Andersen <morten@hjemmesider.dk>
2023-05-23 12:17:52 +0200

    woo Brand
    ACF Brand

--- inc/acf-woocommerce.php
+++ inc/acf-woocommerce.php
@@ -123,3 +123,79 @@ if (function_exists('acf_add_local_field_group')):
 	));
 
 endif;
+
+add_action('acf/include_fields', function () {
+	if (!function_exists('acf_add_local_field_group')) {
+		return;
+	}
+
+	acf_add_local_field_group(array(
+		'key' => 'group_646c88fe3aa44',
+		'title' => 'Woo Brand',
+		'fields' => array(
+			array(
+				'key' => 'field_646c88feccd0a',
+				'label' => 'Brand',
+				'name' => 'brand',
+				'aria-label' => '',
+				'type' => 'true_false',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'message' => '',
+				'default_value' => 0,
+				'ui_on_text' => 'Ja',
+				'ui_off_text' => 'Nej',
+				'ui' => 1,
+			),
+			array(
+				'key' => 'field_646c8c0d101b5',
+				'label' => 'Brand logo',
+				'name' => 'brand_logo',
+				'aria-label' => '',
+				'type' => 'image',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'return_format' => 'id',
+				'library' => 'all',
+				'min_width' => '',
+				'min_height' => '',
+				'min_size' => '',
+				'max_width' => '',
+				'max_height' => '',
+				'max_size' => '',
+				'mime_types' => '',
+				'preview_size' => 'medium',
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'taxonomy',
+					'operator' => '==',
+					'value' => 'product_tag',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+		'show_in_rest' => 0,
+	));
+});
--- template-parts/aside/woo-aside-left.php
+++ template-parts/aside/woo-aside-left.php
@@ -4,14 +4,25 @@ echo '<button id="woo-filter-toggle">Filter</button>';
 echo '<div class="woo-aside-con">';
 dynamic_sidebar('woo-aside');
 
-$product_tags = get_terms('product_tag');
-echo '<div class="widget woo-brands"><h4>Brands</h4>';
-echo '<ul class="woo-tags">';
-foreach ($product_tags as $tag) {
-	$tag_link = get_tag_link($tag->term_id);
-	echo '<li class="' . $tag->slug . '"><a href="' . $tag_link . '" title="' . $tag->name . '">' . $tag->name . '</a></li>';
+if (class_exists('ACF')) {
+	$product_tags = get_terms('product_tag');
+	echo '<div class="widget woo-brands"><h4>Brands</h4>';
+	echo '<ul class="woo-tags grid gap-1">';
+	foreach ($product_tags as $tag) {
+		$tag_link = get_tag_link($tag->term_id);
+
+		if (get_field('brand', $tag)) {
+			$image = get_field('brand_logo', $tag);
+			echo '<li class="' . $tag->slug . '"><a href="' . $tag_link . '" title="' . $tag->name . '">';
+			if (!empty($image)):
+				$size = 'medium';
+				echo wp_get_attachment_image($image, $size);
+			endif;
+			echo '<span>' . $tag->name . '</span></a></li>';
+		}
+	}
+	echo '</ul></div>';
 }
-echo '</ul></div>';
 
 echo '</div>';
 echo '</aside>';
