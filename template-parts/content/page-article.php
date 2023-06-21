6e5b6417fb7b52b0ff21396dcdb90ad34907dccd
Morten Andersen <morten@hjemmesider.dk>
2023-06-21 13:48:33 +0200

    woo check og kurv
    h1 væk

--- template-parts/content/page-article.php
+++ template-parts/content/page-article.php
@@ -1,63 +1,63 @@
-<?php 
+<?php
 
 echo '<main class="page-content">';
- if ( is_page_template( 'page-topimg.php' ) ) {
+if (is_page_template('page-topimg.php')) {
 	web_topimg();
 	web_front_fokus();
 	webspeed_breadcrumb();
 	echo '<div class="wrap">';
-		echo '<article>';
-			while (have_posts()) : the_post();
-				$show_h1  = get_theme_mod( 'webspeed_h1' );
-				if ( $show_h1 ) {
-					web_title();
-				}
-				the_content();
-			endwhile;
-		echo '</article>';
+	echo '<article>';
+	while (have_posts()): the_post();
+		$show_h1 = get_theme_mod('webspeed_h1');
+		if ($show_h1) {
+			web_title();
+		}
+		the_content();
+	endwhile;
+	echo '</article>';
 	echo '</div>';
-} 
-
-elseif ( is_page_template( 'page-normal-titleimg.php' ) ) {
+} elseif (is_page_template('page-normal-titleimg.php')) {
 	echo '<div class="wrap">';
-		web_small_topimg();
-		webspeed_breadcrumb();
-		echo '<article>';
-			while (have_posts()) : the_post();
-				$show_h1  = get_theme_mod( 'webspeed_h1' );
-				if ( $show_h1 ) {
-					web_title();
-				}
-				the_content();
-			endwhile; 
-		echo '</article>';
+	web_small_topimg();
+	webspeed_breadcrumb();
+	echo '<article>';
+	while (have_posts()): the_post();
+		$show_h1 = get_theme_mod('webspeed_h1');
+		if ($show_h1) {
+			web_title();
+		}
+		the_content();
+	endwhile;
+	echo '</article>';
 	echo '</div>';
-} 
+} elseif (is_singular('tribe_events')) {
 
-elseif ( is_singular( 'tribe_events' ) ) {
-	
-		while (have_posts()) : the_post();
-			the_content();
-		endwhile; 
+	while (have_posts()): the_post();
+		the_content();
+	endwhile;
 
-}
-
-else {
+} else {
 	web_front_fokus();
 	webspeed_breadcrumb();
 	echo '<div class="wrap">';
-		echo '<article>';
-			while (have_posts()) : the_post();
+	echo '<article>';
+	while (have_posts()): the_post();
+		if (class_exists('WooCommerce')) {
+			if (!is_checkout() && !is_cart()) {
 				web_title();
-				web_img();
-				the_content();
-			endwhile; 
-
-	 	if ( is_page() && $post->post_parent ) {
-    				web_go_back();
+			}
+		} else {
+			web_title();
 		}
-		
-		echo '</article>';
+		web_img();
+		the_content();
+	endwhile;
+
+	if (is_page() && $post->post_parent) {
+		web_go_back();
+	}
+
+	echo '</article>';
 	echo '</div>';
 }
 echo '</main>';
