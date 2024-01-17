493bd9bdc647a4b18ddb22368469d66ef7bd6ef7
Morten Andersen <morten@hjemmesider.dk>
2024-01-17 13:49:57 +0100

    acf
    test sikkerhed

--- template-parts/aside/aside-left.php
+++ template-parts/aside/aside-left.php
@@ -1,22 +1,22 @@
 <?php
-if( class_exists('ACF') ) {
-    echo '<aside class="left-aside">';
-        dynamic_sidebar('aside-left');
-        the_field('aside_left');
-    echo '</aside>';
+if (class_exists('ACF')) {
+	echo '<aside class="left-aside">';
+	dynamic_sidebar('aside-left');
+	echo wp_kses_post(get_field('aside_left'));
+	echo '</aside>';
 } else {
-    echo '<aside class="left-aside">';
+	echo '<aside class="left-aside">';
 
-       if ( get_post_meta( get_the_ID(), 'aside-left', true ) ) {
-            echo '<div class="widget aside-widget meta-field">';
-                echo get_post_meta( get_the_ID(), 'aside-left', true);
-            echo '</div>';
-       } 
+	if (get_post_meta(get_the_ID(), 'aside-left', true)) {
+		echo '<div class="widget aside-widget meta-field">';
+		echo get_post_meta(get_the_ID(), 'aside-left', true);
+		echo '</div>';
+	}
 
-       if ( get_post_meta( get_the_ID(), 'shortcode-left', true ) ) {
-            $shortcode = get_post_meta( get_the_ID(), 'shortcode-left', true );
-            echo do_shortcode( "{$shortcode}" );
-        }
-        dynamic_sidebar('aside-left');
-    echo '</aside>';
+	if (get_post_meta(get_the_ID(), 'shortcode-left', true)) {
+		$shortcode = get_post_meta(get_the_ID(), 'shortcode-left', true);
+		echo do_shortcode("{$shortcode}");
+	}
+	dynamic_sidebar('aside-left');
+	echo '</aside>';
 }
--- template-parts/aside/aside-right.php
+++ template-parts/aside/aside-right.php
@@ -1,23 +1,23 @@
 <?php
-if( class_exists('ACF') ) {
-    echo '<aside class="right-aside">';
-        dynamic_sidebar('aside-right');
-        the_field('aside_right');
-    echo '</aside>';
+if (class_exists('ACF')) {
+	echo '<aside class="right-aside">';
+	dynamic_sidebar('aside-right');
+	echo wp_kses_post(get_field('aside_right'));
+	echo '</aside>';
 } else {
-    echo '<aside class="right-aside">';
-        
-       if ( get_post_meta( get_the_ID(), 'aside-right', true ) ) {
-            echo '<div class="widget aside-widget meta-field">';
-                echo get_post_meta( get_the_ID(), 'aside-right', true);
-            echo '</div>';
-       } 
+	echo '<aside class="right-aside">';
 
-        if ( get_post_meta( get_the_ID(), 'shortcode-right', true ) ) {
-            $shortcode = get_post_meta( get_the_ID(), 'shortcode-right', true );
-            echo do_shortcode( "{$shortcode}" );
-        }
+	if (get_post_meta(get_the_ID(), 'aside-right', true)) {
+		echo '<div class="widget aside-widget meta-field">';
+		echo get_post_meta(get_the_ID(), 'aside-right', true);
+		echo '</div>';
+	}
 
-        dynamic_sidebar('aside-right');
-    echo '</aside>';
+	if (get_post_meta(get_the_ID(), 'shortcode-right', true)) {
+		$shortcode = get_post_meta(get_the_ID(), 'shortcode-right', true);
+		echo do_shortcode("{$shortcode}");
+	}
+
+	dynamic_sidebar('aside-right');
+	echo '</aside>';
 }
