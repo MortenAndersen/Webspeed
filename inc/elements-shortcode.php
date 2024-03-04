670337d00fd8cdff172062b3d2292d8a8be009d0
Morten Andersen <morten@hjemmesider.dk>
2024-03-04 13:25:21 +0100

    samme type indlæg
    Ændring af single.pph

--- inc/elements-shortcode.php
+++ inc/elements-shortcode.php
@@ -3,52 +3,51 @@
 /* -------------------------------------- */
 add_shortcode('elements', 'webspeed_elements');
 function webspeed_elements($atts) {
-    global $post;
-    ob_start();
+	global $post;
+	ob_start();
 
-    // define attributes and their defaults
-    extract(shortcode_atts(array('grid' => '1', 'gap' =>'0', 'type' => 'none', 'class' => 'no-class'), $atts));
+	// define attributes and their defaults
+	extract(shortcode_atts(array('grid' => '1', 'gap' => '0', 'type' => 'none', 'class' => 'no-class'), $atts));
 
-    require get_parent_theme_file_path('/inc/grid-gap.php');
-
-
-/* -------------------------------------- */
-
-    $loop = new WP_Query( array(
-        'post_type' => 'elements',
-        'orderby' => 'menu_order',
-        'order' => 'ASC',
-        'posts_per_page' => -1,
-        'tax_query' => array(
-            array (
-                'taxonomy' => 'webspeeed_element_type',
-                'field' => 'slug',
-                'terms' => $type,
-            )
-        ),
-    )
-);
+	require get_parent_theme_file_path('/inc/grid-gap.php');
 
 /* -------------------------------------- */
 
-if ( $loop->have_posts() ) {
- 
-	   echo '<div class="grid' . $grid_class . $gap_class . $type . ' ' . $class . '">';
+	$loop = new WP_Query(array(
+		'post_type' => 'elements',
+		'orderby' => 'menu_order',
+		'order' => 'ASC',
+		'posts_per_page' => -1,
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'webspeeed_element_type',
+				'field' => 'slug',
+				'terms' => $type,
+			),
+		),
+	)
+	);
 
+/* -------------------------------------- */
 
-	while ( $loop->have_posts() ) : $loop->the_post();
-        echo '<div class="grid-item element">';
-            web_thumbnail();
-            the_content();
-            web_edit_link();
-        echo '</div>';
+	if ($loop->have_posts()) {
 
-    endwhile; wp_reset_query();
-    echo '</div>';
-}
+		echo '<div class="grid' . $grid_class . $gap_class . $type . ' ' . $class . '">';
 
-    $myvariable = ob_get_clean();
-    return $myvariable;
+		while ($loop->have_posts()): $loop->the_post();
+			echo '<div class="grid-item element">';
+			web_thumbnail();
+			the_content();
+			web_edit_link();
+			echo '</div>';
+
+		endwhile;
+		wp_reset_query();
+		echo '</div>';
+	}
+
+	$myvariable = ob_get_clean();
+	return $myvariable;
 }
 /* -------------------------------------- */
 /* -------------------------------------- */
--- template-parts/aside/aside-single.php
+++ template-parts/aside/aside-single.php
@@ -1,6 +1,58 @@
 <?php
 echo '<aside class="right-aside">';
 
-    dynamic_sidebar('aside-single');
+dynamic_sidebar('aside-single');
+
+$loop = new WP_Query(array(
+	'post_type' => 'post',
+	'posts_per_page' => 3,
+	'category__in' => wp_get_post_categories($post->ID),
+	'post__not_in' => array($post->ID),
+));
+
+/* -------------------------------------- */
+
+if ($loop->have_posts()) {
+
+	echo '<div class="web-loop-posts grid g-d-1 gap-1">';
+	while ($loop->have_posts()): $loop->the_post();
+		$classes = get_post_class('', $post->ID);
+		echo '<div class="post-loop ' . esc_attr(implode(' ', $classes)) . '">';
+
+		// thumbnail
+		if (has_post_thumbnail()) {
+			echo '<div class="loop-post-img">';
+
+			echo '<div class="img-zoom">';
+			echo '<a href="' . get_the_permalink() . '" aria-label="' . get_the_title() . '">';
+			the_post_thumbnail('webspeed-post');
+			echo '</a>';
+			echo '</div>';
+
+			echo '</div>';
+		}
+		echo '<div class="loop-post-txt">';
+		// Date
+
+		web_post_date();
+
+		// Title
+
+		echo '<h4 class="loop-title"><a href="' . get_the_permalink() . '" aria-label="' . get_the_title() . '">' . get_web_title() . '</a></h4>';
+
+		// the_excerpt
+
+		web_excerpt();
+
+		// Read more
+
+		web_read_more();
+
+		echo '</div>';
+		echo '</div>';
+	endwhile;
+	wp_reset_query();
+	echo '</div>';
+}
 
 echo '</aside>';
