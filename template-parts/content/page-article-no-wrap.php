e312ab64b3d890d0221702418463591c3347610a
Morten Andersen <morten@hjemmesider.dk>
2024-05-06 11:24:06 +0200

    Update page-article-no-wrap.php
    webspeed_breadcrumb()

--- template-parts/content/page-article-no-wrap.php
+++ template-parts/content/page-article-no-wrap.php
@@ -3,8 +3,8 @@ echo '<main class="page-content">';
 if ( is_page_template( 'page-no-wrap-topimg.php' ) ) {
  	web_topimg();
  	web_front_fokus();
- 	webspeed_breadcrumb();
 }
+webspeed_breadcrumb();
 	while (have_posts()) : the_post();
 		the_content();
 	endwhile; 
