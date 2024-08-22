c67d43a1302477afc424d627a149a53ae312847e
Morten Andersen <morten@hjemmesider.dk>
2024-08-22 10:33:42 +0200

    woo brands
    woo brand er slettet

--- template-parts/aside/woo-aside-left.php
+++ template-parts/aside/woo-aside-left.php
@@ -1,27 +1,7 @@
 <?php
 echo '<aside class="left-woo-aside">';
-echo '<button id="woo-filter-toggle">Shop kategorier</button>';
-echo '<div class="woo-aside-con">';
-dynamic_sidebar('woo-aside');
-
-if (class_exists('ACF')) {
-	$product_tags = get_terms('product_tag');
-	echo '<div class="widget woo-brands"><h4>Brands</h4>';
-	echo '<ul class="woo-tags grid gap-1">';
-	foreach ($product_tags as $tag) {
-		$tag_link = get_tag_link($tag->term_id);
-
-		if (get_field('brand', $tag)) {
-			$image = get_field('brand_logo', $tag);
-			echo '<li class="' . $tag->slug . '"><a href="' . $tag_link . '" title="' . $tag->name . '">';
-			if (!empty($image)):
-				$size = 'medium';
-				echo wp_get_attachment_image($image, $size);
-			endif;
-			echo '<span>' . $tag->name . '</span></a></li>';
-		}
-	}
-	echo '</ul></div>';
-}
-echo '</div>';
+	echo '<button id="woo-filter-toggle">Shop kategorier</button>';
+	echo '<div class="woo-aside-con">';
+		dynamic_sidebar('woo-aside');
+	echo '</div>';
 echo '</aside>';
