7386ad7d1f1588a2ad90ac594eb52f6133204829
Morten Andersen <morten@hjemmesider.dk>
2024-11-27 12:54:52 +0100

    Update blog-loop.php
    Mulighed for indledningstekst på blog

--- template-parts/content/blog-loop.php
+++ template-parts/content/blog-loop.php
@@ -1,38 +1,43 @@
 <?php
 web_topimg_blog();
 
-function blog_content() {
-
-	web_blog_thumbnail();
-	web_date_cat();
-	echo '<h2 class="loop-title"><a href="' . get_permalink() . '">' . get_web_title() . '</a></h2>';
-	web_excerpt();
-	web_read_more();
-
+// Indhold på selve blog siden printes
+$post = get_queried_object();
+setup_postdata( $post );
+$blogBody = get_the_content();
+if ($blogBody) {
+	echo '<div class="wrap" style="padding-block:0;">';
+		the_content();
+	echo '</div>';
 }
+wp_reset_postdata();
 
+// De enkelte blog indlæg
 echo '<div class="wrap grid g-d-3 gap-2">';
 
 if (have_posts()): while (have_posts()): the_post();
 		$classes = get_post_class('', $post->ID);
 		echo '<article class="post-loop ' . esc_attr(implode(' ', $classes)) . '">';
-
-		blog_content();
-
+			web_blog_thumbnail();
+			web_date_cat();
+			echo '<h2 class="loop-title"><a href="' . get_permalink() . '">' . get_web_title() . '</a></h2>';
+			web_excerpt();
+			web_read_more();
 		echo '</article>';
 	endwhile;
 	wp_reset_query();
-	// Pagination
+
 	echo '</div>';
 
-	echo '<div class="blog-nav wrap flex gap-2">';
+// Pagination
+echo '<div class="blog-nav wrap flex gap-2">';
 	echo '<div class="nav-previous">';
-	previous_posts_link('<<');
+		previous_posts_link('<<');
 	echo '</div>';
 	echo '<div class="nav-next">';
-	next_posts_link('>>');
+		next_posts_link('>>');
 	echo '</div>';
-	echo '</div>';
+echo '</div>';
 
 endif;
 
